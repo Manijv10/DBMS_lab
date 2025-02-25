1f511f5a753f84a1313145dd396de34229d2b010
Manijv10 <139250900+Manijv10@users.noreply.github.com>
2025-02-25 14:49:05 +0530

    Update StaticBuffer.cpp

--- mynitcbase/Buffer/StaticBuffer.cpp
+++ mynitcbase/Buffer/StaticBuffer.cpp
@@ -1,2 +1,54 @@
+//stage 3
+
 #include "StaticBuffer.h"
 #include <iostream>
+unsigned char StaticBuffer::blocks[BUFFER_CAPACITY][BLOCK_SIZE];
+struct BufferMetaInfo StaticBuffer::metainfo[BUFFER_CAPACITY];
+
+StaticBuffer::StaticBuffer() {
+
+  // initialise all blocks as free
+  for (int i=0;i<BUFFER_CAPACITY;i++) {
+    metainfo[i].free = true;
+  }
+}
+
+
+//At this stage, we are not writing back from the buffer to the disk since we are
+//not modifying the buffer. So, we will define an empty destructor for now. In
+//subsequent stages, we will implement the write-back functionality here.
+
+StaticBuffer::~StaticBuffer() {}
+
+int StaticBuffer::getFreeBuffer(int blockNum) {
+  if (blockNum < 0 || blockNum >DISK_BLOCKS) {
+    return E_OUTOFBOUND;
+  }
+  int allocatedBuffer=-1;
+  for(int i=0;i<BUFFER_CAPACITY;i++){
+    if(metainfo[i].free==true){
+        allocatedBuffer=i;
+        break;
+    }
+  }
+  metainfo[allocatedBuffer].free = false;
+  metainfo[allocatedBuffer].blockNum = blockNum;
+  
+
+  return allocatedBuffer;
+}
+
+int StaticBuffer::getBufferNum(int blockNum) {
+  if(blockNum<0 || blockNum >DISK_BLOCKS){
+    return E_OUTOFBOUND;
+  }
+
+  for(int bufferindex=0;bufferindex<BUFFER_CAPACITY;bufferindex++){
+    if(metainfo[bufferindex].blockNum==blockNum){
+      return bufferindex;
+    }
+  }
+
+
+    return E_BLOCKNOTINBUFFER;
+}
