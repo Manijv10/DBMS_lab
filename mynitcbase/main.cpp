d78e0e5c7d7ae83dd77b88a51676e44612636e28
Manijv10 <139250900+Manijv10@users.noreply.github.com>
2025-02-25 14:56:38 +0530

    Update main.cpp

--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -2,30 +2,15 @@
 #include "Cache/OpenRelTable.h"
 #include "Disk_Class/Disk.h"
 #include "FrontendInterface/FrontendInterface.h"
+#include <stdio.h>
 #include <iostream>
+#include <cstring>
 
-
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
+  // Initialize the Run Copy of Disk 
   Disk disk_run;
   StaticBuffer buffer;
-  //update_names();
   OpenRelTable cache;
 
-  for(int i=0;i<=2;i++){
-    RelCatEntry relCatEntry;
-    int getRelResult = RelCacheTable::getRelCatEntry(i,&relCatEntry);
-   if(getRelResult == SUCCESS){
-    printf("Relation: %s\n",relCatEntry.relName);
-    for(int j=0;j<relCatEntry.numAttrs;j++){
-        AttrCatEntry attrcatEntry;
-        int getAttrResult = AttrCacheTable::getAttrCatEntry(i,j,&attrcatEntry);
-        if(getAttrResult == SUCCESS){
-            const char* attrType = attrcatEntry.attrType== NUMBER ? "NUM" : "STR";
-            printf(" %s: %s\n", attrcatEntry.attrName, attrType); 
-        }
-    }
-  }
-  printf("\n");
-}
-return 0;
+  return FrontendInterface::handleFrontend(argc, argv);
 }
