3345d1f111c5c0fb7c0d5bb542399f23fc282d9d
Manijv10 <139250900+Manijv10@users.noreply.github.com>
2025-02-25 11:59:23 +0530

    Update main.cpp

--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -1,26 +1,66 @@
-//stage 1
 #include "Buffer/StaticBuffer.h"
 #include "Cache/OpenRelTable.h"
 #include "Disk_Class/Disk.h"
 #include "FrontendInterface/FrontendInterface.h"
 #include <iostream>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+    Disk disk_run;
 
-  Disk disk_run;
-  unsigned char buffer[BLOCK_SIZE];
-  char message1[]="hello";
-  Disk ::readBlock(buffer,7000); //read from block && load in buffer
-  memcpy(buffer+20,message1,5); //modifying the buffer from the position 20 with new words
-  Disk :: writeBlock(buffer,7000);  //write modified buffer content into disk
-  
-  unsigned char buffer2[BLOCK_SIZE];
-  char message2[6];
-  Disk::readBlock(buffer2,7000); //read the same block and load into buffer2
-  memcpy(message2,buffer2 + 20,6);//cpy from buffer2+20 onwards into an array message2
-  std::cout <<message2;
-  
+    RecBuffer relCatBuffer(RELCAT_BLOCK);
+    RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
 
-  return 0;
+    HeadInfo relCatHeader;
+    HeadInfo attrCatHeader;
+
+    relCatBuffer.getHeader(&relCatHeader);
+    attrCatBuffer.getHeader(&attrCatHeader);
+
+    for (int i = 0; i < relCatHeader.numEntries; i++)
+    {
+
+        Attribute relCatRecord[RELCAT_NO_ATTRS];
+        relCatBuffer.getRecord(relCatRecord, i);
+        
+
+        printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+        for (int j = 0; j < attrCatHeader.numEntries; j++)
+        {
+            Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+            attrCatBuffer.getRecord(attrCatRecord, j);
+
+            if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal,relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0) { //DO NOT WRITE [ATTRCAT_ATTR_NAME_INDEX]
+      		if(strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal,"Student")==0){
+      			if(strcmp(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal,"Class")==0){
+      				strcpy(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal,"Batch");
+      			}
+      		}
+      		if(strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal,"Events")==0){
+      			if(strcmp(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal,"location")==0){
+      				strcpy(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal,"CT");
+      			}
+      		}
+      		
+
+      		
+                const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+                printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+                if (j == (attrCatHeader.numSlots) - 1 && attrCatHeader.rblock != -1)
+                { // that is it crossed one block
+                    attrCatBuffer = RecBuffer(attrCatHeader.rblock);
+                    attrCatBuffer.getHeader(&attrCatHeader);
+ 		            j=-1;
+                }
+          
+           }
+        }
+        
+        printf("\n");
+   }
+    
+
+    return 0;
 }
-
