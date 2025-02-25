e3c1cc40aaca4fa246d2e55611c1b736e074a29b
Manisha John Varghese <139250900+Manijv10@users.noreply.github.com>
2025-02-25 18:41:32 +0530

    Update Schema.cpp

--- mynitcbase/Schema/Schema.cpp
+++ mynitcbase/Schema/Schema.cpp
@@ -35,3 +35,30 @@ int Schema::openRel(char relName[ATTR_SIZE]) {
     return OpenRelTable::closeRel(relId);
   }
   
+
+  int renameRel(char oldRelName[ATTR_SIZE],char newRelName[ATTR_SIZE]){
+    if(strcmp(oldRelName,RELCAT_RELNAME) == 0 || strcmp(newRelName,RELCAT_RELNAME) == 0 || strcmp(oldRelName,ATTRCAT_RELNAME) == 0 || strcmp(newRelName,ATTRCAT_RELNAME) == 0){
+      return E_NOTPERMITTED;
+  }
+   //relation must be closed to rename .Therefore if it is open we just return .
+  int relId=OpenRelTable::getRelId(oldRelName);
+  if(relId>=0){
+    return E_RELOPEN;
+  }
+  int retVal=BlockAccess::renameRelation(oldRelName,newRelName);
+  return retVal;
+}
+
+
+int Schema::renameAttr(char* relName,char* oldAttrName,char *newAttrName){
+  if(strcmp(relName,RELCAT_RELNAME) == 0 || strcmp(relName,ATTRCAT_RELNAME) == 0){
+    return E_NOTPERMITTED;
+}
+int relId=OpenRelTable::getRelId(relName);
+  if(relId>=0){
+    return E_RELOPEN;
+  }
+
+  int retVal=BlockAccess::renameAttribute(relName,oldAttrName,newAttrName);
+  return retVal;
+}
